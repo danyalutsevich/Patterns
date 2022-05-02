065c21ee3e49c19b69f49799dd74cc65940a4214
danyalutsevich <luchevich31@gmail.com>
2022-05-02 11:21:10 +0300

    Singleton

---
+++ Patterns/Creational/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Creational
+{
+    class Singleton
+    {
+        public DateTime Moment => DateTime.Now;
+
+        private static Singleton instance;
+        
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+
+        private Singleton()
+        {
+        }
+
+    }
+}
---
+++ Patterns/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Patterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Singleton
+
+            // private ctor blocks creation
+            //var singleton = new Creational.Singleton
+
+            // creation 
+            var singleton1 = Creational.Singleton.GetInstance();
+            Console.WriteLine(singleton1.Moment);
+            var singleton2 = Creational.Singleton.GetInstance();
+            Console.WriteLine(singleton1==singleton2);
+            #endregion
+        }
+    }
+}
